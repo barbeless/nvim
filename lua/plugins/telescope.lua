99a9575380a3eaa16e1416fbea496807983f19bf
Nicolas Leone Mailhot <nmailhot@devolutions.net>
2024-02-01 14:15:35 -0500

    start of nvim config

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.5",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").setup({
+
+	defaults = {
+		hidden = false,
+		file_ignore_patterns = {".git\\", ".cache", "%.o", "%.a", "%.out", "%.class",
+		"%.pdf", "%.mkv", "%.mp4", "%.zip"},
+	},
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
+      })
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+      require("telescope").load_extension("ui-select")
+    end,
+  },
+}
