91dc314626f106e6b8872ff3db74e4d478bf7d94
Nicolas Leone Mailhot <nmailhot@devolutions.net>
2024-02-02 08:36:42 -0500

    new theme, still trying to add lsp and more QOL options

--- init.lua
+++ init.lua
@@ -11,3 +11,6 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("plugins")
+require("mason").setup()
+local lspconfig = require('lspconfig')
+ vim.wo.relativenumber = true
